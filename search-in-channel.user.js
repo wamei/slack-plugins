efcd79855ade642b0f39f574213d404a641050c3
wamei <wamei.cho@gmail.com>
2018-04-21 18:48:06 +0900

    endpoint追加

---
+++ search-in-channel.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Wamei Slack Search In Channel Plugin
+// @namespace    wamei
+// @version      0.1
+// @description  お手軽にチャンネル内検索出来るようにする
+// @author       wamei
+// @match        https://*.slack.com/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const inject = (src) => {
+        $('body').prepend(`<script src="${src}">`);
+    };
+
+    inject("https://wamei.github.io/slack-plugins/dist/search-in-channel.js");
+})();
--- src/plugins.js
+++ src/plugins.js
@@ -8,5 +8,6 @@
     inject("https://wamei.github.io/slack-plugins/dist/inline-image.js");
     inject("https://wamei.github.io/slack-plugins/dist/mention.js");
     inject("https://wamei.github.io/slack-plugins/dist/reply.js");
+    inject("https://wamei.github.io/slack-plugins/dist/search-in-channel.js");
     inject("https://wamei.github.io/slack-plugins/dist/suspend-typing-status.js");
 })();
