ff8ac784a60d8978a7a18cd92c02efb64f549bd2
wamei <wamei.cho@gmail.com>
2019-11-07 16:26:06 +0900

    build

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ class CreateLoaderPlugin {
                     `// ==UserScript==
 // @name         wamei-${key}
 // @namespace    wamei
-// @version      0.5
+// @version      0.6
 // @author       wamei
 // @match        https://*.slack.com/*
 // ==/UserScript==`,
