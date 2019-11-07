a277e85e1415d64313106181733a619ee9f7b77b
wamei <wamei.cho@gmail.com>
2019-11-07 16:05:07 +0900

    ダークモード対応

--- src/mention.js
+++ src/mention.js
@@ -3,11 +3,20 @@ import Util from './class/util.js';
 (function() {
     'use strict';
 
-    const mentionColor = '#ddebd7';
-    const mentionHoverColor = '#e7efe4';
+    const mentionColorLight = '#ddebd7';
+    const mentionHoverColorLight = '#e7efe4';
+    const mentionColorDark = '#2f4626';
+    const mentionHoverColorDark = '#436337';
     const mention = (message, target) => {
         if (!message.querySelector(target)) {
             return;
+        }
+        let mentionColor = mentionColorLight;
+        let mentionHoverColor = mentionHoverColorLight;
+        const color = window.getComputedStyle(document.body).borderBlockEndColor.match(/rgba?\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)/);
+        if (color[1] > 100 && color[2] > 100 && color[3] > 100) {
+            mentionColor = mentionColorDark;
+            mentionHoverColor = mentionHoverColorDark;
         }
         message.style.backgroundColor = mentionColor;
         message.addEventListener('mouseover', () => {
