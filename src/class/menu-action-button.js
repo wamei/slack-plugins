3a2a3ff04bc39cf48f0ae3f248eb6831ceab32d1
wamei <wamei.cho@gmail.com>
2019-11-07 15:12:51 +0900

    引用にtooltip表示

--- src/class/menu-action-button.js
+++ src/class/menu-action-button.js
@@ -1,9 +1,10 @@
 import Message from './message.js';
 
 export default class MenuActionButton {
-    constructor(template, callback) {
-        this.template = template;
+    constructor(callback, template, tooltip) {
         this.callback = callback;
+        this.template = template;
+        this.tooltip = tooltip;
     }
 
     isAvailable(message) {
@@ -11,11 +12,30 @@ export default class MenuActionButton {
     }
 
     createElement(input, message) {
-        const $wrap = document.createElement('p');
+        const $wrap = document.createElement('div');
         $wrap.innerHTML = this.template;
         const $element = $wrap.firstElementChild;
+
+        $wrap.innerHTML = this.tooltip;
+        $wrap.style.position = 'relative';
+        const $tooltip = $wrap.firstElementChild;
+        $tooltip.style.position = 'absolute';
+        $tooltip.style.zIndex = '99999';
+        $tooltip.style.display = 'none';
+        $tooltip.style.width = '200px';
+        $element.appendChild($wrap);
         $element.addEventListener('click', () => {
             this.callback(input, message);
+        });
+        $element.addEventListener('mouseover', () => {
+            $tooltip.style.display = 'block';
+            const elementRect = $element.getClientRects()[0];
+            const tooltipRect = $tooltip.getClientRects()[0];
+            $tooltip.style.top = `${-elementRect.height-tooltipRect.height}px`;
+            $tooltip.style.left = `${elementRect.width-(tooltipRect.width/2)}px`;
+        });
+        $element.addEventListener('mouseleave', () => {
+            $tooltip.style.display = 'none';
         });
         return $element;
     }
